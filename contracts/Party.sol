43e92227ee681da1150c130fe8c01f19472c446a
Boboye-Ak <boboye771@gmail.com>
2022-10-30 12:35:53 +0100

    wrote checkIn and grant/revokeAuthorisation functions

--- contracts/Party.sol
+++ contracts/Party.sol
@@ -8,20 +8,33 @@ contract Party is ERC721 {
     uint256 immutable totalSupply;
     uint256 s_tokenId;
     uint256 immutable i_cost;
+    address immutable i_owner;
     mapping(uint256 => bool) isCheckedIn;
+    mapping(address => bool) isAuthorized;
 
     //Custom Errors
     error Party__SendWithTicketCost();
     error Party__SoldOut();
+    error Party__Unauthorized();
+
+    //modifiers
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) {
+            revert Party__Unauthorized();
+        }
+        _;
+    }
 
     constructor(
         string memory name,
         string memory symbol,
         uint256 maxAttendees,
-        uint256 cost
+        uint256 cost,
+        address owner
     ) ERC721(name, symbol) {
         totalSupply = maxAttendees;
         i_cost = cost;
+        i_owner = owner;
     }
 
     //Receive and Fallback Functions
@@ -37,12 +50,36 @@ contract Party is ERC721 {
         if (msg.value < i_cost) {
             revert Party__SendWithTicketCost();
         }
-        if (s_tokenId>=totalSupply){
+        if (s_tokenId >= totalSupply) {
             revert Party__SoldOut();
-
         }
 
         s_tokenId += 1;
-        _safeMint(msg.sender, tokenId);
+        _safeMint(msg.sender, s_tokenId);
+        return s_tokenId;
+    }
+
+    function grantAuthorization(address grantee) external onlyOwner {
+        if (isAuthorized[grantee]) {
+            revert();
+        }
+        isAuthorized[grantee] = true;
+    }
+
+    function revokeAuthorization(address grantee) external onlyOwner {
+        if (!isAuthorized[grantee]) {
+            revert();
+        }
+        isAuthorized[grantee] = false;
+    }
+
+    function checkIn(uint256 tokenId) external {
+        if ((msg.sender != i_owner) && (!isAuthorized[msg.sender])) {
+            revert Party__Unauthorized();
+        }
+        if (isCheckedIn[tokenId]) {
+            revert();
+        }
+        isCheckedIn[tokenId] = true;
     }
 }
