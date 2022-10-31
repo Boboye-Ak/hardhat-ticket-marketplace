f4efaeca8c6fc5dce317bded577a1ebca410305d
Boboye-Ak <boboye771@gmail.com>
2022-10-31 20:40:46 +0100

    Wrote before each function of unit test

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -6,21 +6,24 @@ const networkConfig = {
         percentCut: "300",
         partyName: "MainlandBlockParty",
         symbol: "MBP",
-        maxAttendees: "1000",cost:ethers.utils.parseEther("0.01")
+        maxAttendees: "1000",
+        cost: ethers.utils.parseEther("0.01"),
     },
     31337: {
         name: "hardhat",
         percentCut: "300",
         partyName: "MainlandBlockParty",
         symbol: "MBP",
-        maxAttendees: "1000",cost:ethers.utils.parseEther("0.01")
+        maxAttendees: "1000",
+        cost: ethers.utils.parseEther("0.01"),
     },
     5: {
         name: "goerli",
         percentCut: "300",
         partyName: "MainlandBlockParty",
         symbol: "MBP",
-        maxAttendees: "1000",cost:ethers.utils.parseEther("0.01")
+        maxAttendees: "1000",
+        cost: ethers.utils.parseEther("0.01"),
     },
 }
 
--- test/unit tests/Party.test.js
+++ test/unit tests/Party.test.js
@@ -6,4 +6,19 @@ const { assert, expect } = require("chai")
 
 !developmentChains.includes(network.name)
     ? describe.skip
-    : describe("Party", () => {})
+    : describe("Party", () => {
+          let partyFactory, deployer, host, party, partyAddress
+          const percentCut = networkConfig[chainId]["percentCut"]
+          const partyName = networkConfig[chainId]["partyName"]
+          const symbol = networkConfig[chainId]["symbol"]
+          const maxAttendees = networkConfig[chainId]["maxAttendees"]
+          const cost = networkConfig[chainId]["cost"]
+          beforeEach(async () => {
+              await deployments.fixture(["deployment"])
+              deployer = (await hre.getNamedAccounts()).deployer
+              host = (await ethers.getSigners())[1]
+              partyFactory = await ethers.getContract("PartyFactory", host)
+              partykAddress = (await partyFactory.getParties(host.address))[0]
+              party = await ethers.getContractAt("Party", partyAddress, host)
+          })
+      })
